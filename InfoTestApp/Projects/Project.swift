0ecc788985d7212bff0752f6841ab793afd82937
junha <goodjunha@gmail.com>
2023-04-12 15:34:05 +0900

    커미ㅣㅅ

--- InfoTestApp/Projects/Project.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Project.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by 박준하 on 2023/04/12.
-//
-
-import Foundation
---
+++ InfoTestApp/Workspace.swift
@@ -0,0 +1,12 @@
+//
+//  Workspace.swift
+//  ProjectDescriptionHelpers
+//
+//  Created by 박준하 on 2023/04/12.
+//
+
+import ProjectDescription
+
+let appName = "Info-iOS"
+
+let workspace = Workspace(name: appName, projects: ["Projects/*"])
